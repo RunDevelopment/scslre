76fb27763bd1839a1ea1fdbe7dcda94f114bc8fd
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-04-09 20:17:25 +0200

    Fixed build

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { terser } from "rollup-plugin-terser";
 export default /** @type {import('rollup').RollupOptions[]} */ ([
 	{
 		input: ".out/index.js",
-		external: ["regexpp", "refa"],
+		external: ["regexpp", "refa", "regexp-ast-analysis"],
 		output: {
 			file: "index.js",
 			format: "cjs",
